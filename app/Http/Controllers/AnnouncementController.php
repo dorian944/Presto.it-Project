d5e85369cb7cf612042c7b221485fb25594b216d
Giuliana D'aquino <giulianadaquino@yahoo.it>
2024-05-03 19:01:41 +0200

    middleware annunci

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -9,7 +9,7 @@ class AnnouncementController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('indexAnnouncement');
     }
 
     public function createAnnouncement(){
