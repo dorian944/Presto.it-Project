440acaccd6f442956b6cb4c2687c4fadeac56df3
Giuliana D'aquino <giulianadaquino@yahoo.it>
2024-05-07 19:19:18 +0200

    migrazione table image

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -30,4 +30,9 @@ class Announcement extends Model
     public static function toBeRevisionedCount(){
         return Announcement::where('is_accepted', null)->count();
     }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
 }
