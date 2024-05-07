45b0d2d3e8572028b945f0361a36ddb31bf779d9
Giuliana D'aquino <giulianadaquino@yahoo.it>
2024-05-07 20:04:24 +0200

    preview immagini

--- app/Livewire/CreateAnnouncement.php
+++ app/Livewire/CreateAnnouncement.php
@@ -7,9 +7,12 @@ use App\Models\Category;
 use App\Models\Announcement;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Validate;
+use Livewire\WithFileUploads;
 
 class CreateAnnouncement extends Component
 {
+    use WithFileUploads;
+
     #[Validate('required|min:3|max:50')] 
     public $title;
 
@@ -22,14 +25,26 @@ class CreateAnnouncement extends Component
     #[Validate('required')]
     public $category;
    
+    public $images = [];
+    public $image;
+    public $temporary_images;
 
+    protected $rules = [
+        'images.*'=>'image|max:1024',
+        'temporary_images.*'=> 'image|max:1024',
+    ];
 
     //per customizzare i messaggi di validazione
     protected $messages = [
         'required' => 'Campo richiesto',
         'min' => 'Il campo deve essere minimo di :min caratteri',
         'max' => 'Il campo deve essere massimo di :max caratteri',
-        'numeric'=>'Il campo deve essere numerico con massimo due decimali'
+        'numeric'=>'Il campo deve essere numerico con massimo due decimali',
+        'temporary_images.required' => 'L\'immagine è richiesta',
+        'temporary_images.*.image' => 'I file devono essere immagini',
+        'temporary_images.*.max' => 'L\'immagine dev\'essere massimo di 5mb',
+        'images.image' => 'L\'immagine dev\'essere massimo un\'immagine',
+        'images.max' => 'L\'immagine dev\'essere massimo di 5mb',
         ];
 
     public function store(){
@@ -50,6 +65,16 @@ class CreateAnnouncement extends Component
         // $this→cleanForm(); fa le stesse cose del reset, ovvero dopo aver registrato il record, azzeriamo nel form i vari input
         $this->reset();
         return redirect()->to(route('announcements.create'));
+    }
+
+    public function updatedTemporaryImages()
+    {
+        if($this->validate([
+            'temporary_images.*'=>'image|max:1024',
+        ]))
+        foreach ($this->temporary_images as $image){
+            $this->images[]=$image;
+        }
     }
 
     public function render()
--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -53,6 +53,33 @@
                         @endforeach
                 </select>
             </div>
+            {{-- fine categorie --}}
+            <div class="mb-3">
+                <input wire:model="temporary_images" type="file" name="images" multiple class="form-control shadow @error('temporary_images.*') is-invalid @enderror" placeholder="Img" />
+                    @error('temporary_images.*')
+                    <p class="text-danger mt-2">{{$message}}</p>
+                    @enderror
+            </div>
+            @if (!empty($images))
+            <div class="row">
+                <div class="col-12">
+                    <p>Photo preview:</p>
+                    <div class="row border border-4 border-info rounded shadow py-4">
+                        @foreach ($images as $key => $image )
+                            <div class="col my-3">
+                                <div class="img-preview-custom mx-auto shadow rounded" style="background-image: url({{$image->temporaryUrl()}});">
+                                </div>
+                                    {{-- <button type="button" class="btn btn-danger shadow d-block text-center mt-2 mx-auto" wire:click="removeImage({{$key}})">Cancella
+                                    </button> --}}
+                            </div>
+                            
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            @endif
+
+
 
 
 
