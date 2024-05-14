79df6c507236789992e2da17c31e2d42af18b683
Dorian Fejza <dori_do@hotmail.com>
2024-05-14 14:27:24 +0200

    user story 8 finito, da testare dai colleghi

--- app/Livewire/CreateAnnouncement.php
+++ app/Livewire/CreateAnnouncement.php
@@ -10,6 +10,7 @@ use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
 use App\Jobs\GoogleVisionSafeSearch;
 use App\Jobs\GoogleVosionLabelImage;
+use App\Jobs\RemoveFaces;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 
@@ -73,9 +74,14 @@ class CreateAnnouncement extends Component
                 $newFileName = "announcements/{$announcement->id}";
                 $newImage = $announcement->images()->create(['path'=>$image->store($newFileName,'public')]);
 
-                dispatch(new ResizeImage($newImage->path, 400, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVosionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+
+                    new ResizeImage($newImage->path, 400, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVosionLabelImage($newImage->id),
+
+                ])->dispatch($newImage->id);
+
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
