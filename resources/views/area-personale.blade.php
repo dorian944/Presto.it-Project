db5e5f3714b75dc3bbed36400a83cde75495d1e1
Filippo Banterle <banterlefilippo04@gmail.com>
2024-05-16 21:23:04 +0200

    sistemata zona utente

--- resources/views/area-personale.blade.php
+++ resources/views/area-personale.blade.php
@@ -1,36 +1,22 @@
 <x-layout>
 
-    <div class="container-fluid custom-view">
-        <div class="row">
-            <div class="col-12">
-                {{-- ("ui.benvenuto") = Benvenuto/a nella tua area personale  --}}
-               <h1 class="text-center mt-5">{{__("ui.benvenuto")}}</h1> 
-            </div>
+  <div class="container-fluid custom-view-profilo ">
+    <div class="row ">
+        <div class="col-12 d-flex  justify-content-center">
+
+            <div class="card mt-5" style="width: 25rem;">
+                <img src="https://cdn.icon-icons.com/icons2/1154/PNG/512/1486564400-account_81513.png" class="card-img-top img-fluid z-3 p-5 " alt="foto utente">
+                <div class="card-body z-3">
+                  <h5 class="card-title text-center">{{__("ui.benvenuto")}}</h5>
+                  <h5 class="text-center">Nome utente : {{Auth::user()->name}}</h5>
+                  <p class="card-text text-center">Email : {{Auth::user()->email}}</p>
+                  <p class="card-text text-center"> {{ Auth::user()->is_revisor ? 'Sono revisore' : 'Non sono revisore' }} </p>
+                </div>
+              </div>
+
         </div>
-
-        {{-- <div class="row justify-content-center">
-            <h1 class="text-center">WORK IN PROGRESS....</h1>
-
-            <img  style="width: 800px;" src="https://static.wixstatic.com/media/0ae27f_10ad9d3946314ac8b4c9ed2a9d0483c6~mv2.png/v1/fill/w_560,h_322,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Work-in-progress.png" alt="work-in-progress">
-        </div> --}}
-
-        {{-- info generali --}}
-        <div class="row my-5">
-            <div class="card-profilo">
-                <h2 class="col-12 d-flex justify-content-center">
-                    Nome utente : {{Auth::user()->name}}
-                </h2>
-
-                <h2 class="col-12 d-flex justify-content-center">
-                     Email : {{Auth::user()->email}}
-                </h2>
-
-                <h2 class="col-12 d-flex justify-content-center">
-                    {{ Auth::user()->is_revisor ? 'Sono revisore' : 'Non sono revisore' }} 
-                </h2>
-            </div>
-        </div>
-
     </div>
+  </div>
+   
 
 </x-layout>
