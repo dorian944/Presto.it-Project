1d29e23fda66d7981ee4f99d840cd038f110ff4a
Filippo Banterle <banterlefilippo04@gmail.com>
2024-05-16 19:59:33 +0200

    effetto card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,11 +1,11 @@
-<div class="card-container margin-custom-card-width-1920">
+<div class="card-container margin-custom-card-width-1920 ">
   <div class="card mb-4 rounded-5" style="width: 20rem; ">
-      <img src="{{!$announcement->images()->get()->isEmpty() ? $announcement->images()->first()->getUrl(400,300) : 'https://picsum.photos/200'}}" class="card-img-top p-3 img-fluid " alt="Immagine articolo">
-      <div class="card-body">
-        <h5 class="card-head">{{$announcement->title}}</h5>
+      <img src="{{!$announcement->images()->get()->isEmpty() ? $announcement->images()->first()->getUrl(400,300) : 'https://picsum.photos/200'}}" class="card-img-top p-3 img-fluid z-3" alt="Immagine articolo">
+      <div class="card-body z-3">
+        <h5 class="card-head ">{{$announcement->title}}</h5>
           <p class="card-text">{{$announcement->body}}</p>
           <p class="card-text">{{$announcement->price}} &euro;</p>
-          <a href="{{route('announcements.show', compact('announcement'))}}" class="btn button-custom">{{__('ui.dettaglio')}}</a> <br>
+          <a href="{{route('announcements.show', compact('announcement'))}}" class="btn button-custom  ">{{__('ui.dettaglio')}}</a> <br>
           <a href="{{route('categoryShow', ['category' => $announcement->category->id])}}" class="btn button-custom my-2"> {{__('ui.Categorie')}}: {{$announcement->category->name}}</a>
           <p class="card-footer">{{__('ui.pubblicato_il')}} {{$announcement->created_at->format('d/m/Y')}}</p>
       </div>
