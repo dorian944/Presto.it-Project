d97a47eef61785022def063f72d1945739322b63
Giuliana D'aquino <giulianadaquino@yahoo.it>
2024-05-17 20:00:13 +0200

    btn inserisci annuncio visibile anche ai guest

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,12 +4,13 @@
                 <h1 class="animated-text-from-right h1-homepage ">PRESTO.IT</h1>
 
                 <h5 class="animated-text-from-left sottotitolo-homepage">{{__("ui.sottotitolo_header")}}</h5>
-    
-                @auth
+
+                {{-- far vedere il btn aggiungi articolo anche ai guest che poi li riporta nella pagina di registrazione --}}
+                {{-- @auth --}}
                     <button class=" button-custom-homepage mt-4">
                          <a href="{{route('announcements.create')}}" class="text-decoration-none text-white testo-bottone-homepage ">{{__("ui.button_header")}}</a>
                     </button>
-                @endauth
+                {{-- @endauth --}}
         </div>
 
 
