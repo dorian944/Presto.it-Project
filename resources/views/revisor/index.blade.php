b772df12834c8ffa922b96a5e0373b5a734bf224
Giuliana D'aquino <giulianadaquino@yahoo.it>
2024-05-20 18:34:44 +0200

    icone carosello visibili

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -106,9 +106,9 @@
                                                     </div>
                                                 </div>
                                             @endif
-                                            <button class="carousel-control-prev" type="button"
+                                            <button class="carousel-control-prev " type="button"
                                                 data-bs-target="#carouselExample" data-bs-slide="prev">
-                                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                <span class="carousel-control-prev-icon " aria-hidden="true"></span>
                                                 <span class="visually-hidden">Previous</span>
                                             </button>
                                             <button class="carousel-control-next" type="button"
